7c588088059757a44f54d9cd447f6ec1c6ce0641
Axion_Hwanje <108977628+Hwanje@users.noreply.github.com>
2026-03-30 13:24:42 +0900

    tae

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ app.add_middleware(
 
 socket_app = socketio.ASGIApp(sio, app)
 
-user_sessions = {}  # { sid: "닉네임" }
-rooms_db = {}       # { "방이름": "OTP_Secret" }
+user_sessions = {}
+rooms_db = {}
 
 @sio.event
 async def connect(sid, environ):
@@ -29,9 +29,9 @@ async def create_room(sid, data):
     if room_name not in rooms_db:
         rooms_db[room_name] = pyotp.random_base32()
     
-    sio.enter_room(sid, room_name)
+    # 수정: await 추가
+    await sio.enter_room(sid, room_name)
     totp = pyotp.TOTP(rooms_db[room_name], interval=60)
-    # 방 생성자에게 코드와 성공 알림 전송
     await sio.emit('display_otp', {'code': totp.now()}, to=sid)
     await sio.emit('join_success', {'room': room_name}, to=sid)
 
@@ -42,19 +42,19 @@ async def join_with_otp(sid, data):
     if room in rooms_db:
         totp = pyotp.TOTP(rooms_db[room], interval=60)
         if totp.verify(otp_code):
-            sio.enter_room(sid, room)
+            # 수정: await 추가
+            await sio.enter_room(sid, room)
             await sio.emit('join_success', {'room': room}, to=sid)
         else:
-            await sio.emit('join_fail', {'msg': "OTP가 틀렸습니다."}, to=sid)
+            await sio.emit('join_fail', {'msg': "OTP가 일치하지 않습니다."}, to=sid)
     else:
-        await sio.emit('join_fail', {'msg': "방이 없습니다."}, to=sid)
+        await sio.emit('join_fail', {'msg': "방을 찾을 수 없습니다."}, to=sid)
 
 @sio.event
 async def set_nickname(sid, data):
     nickname = data.get('nickname', '익명')
     room = data.get('room')
     user_sessions[sid] = nickname
-    # 방 전체에 알림
     await sio.emit('notification', {'msg': f"'{nickname}'님이 입장했습니다."}, room=room)
 
 @sio.event
@@ -62,7 +62,6 @@ async def refresh_otp(sid, data):
     room = data.get('room')
     if room in rooms_db:
         totp = pyotp.TOTP(rooms_db[room], interval=60)
-        # 갱신된 코드를 '방 전체'가 아닌 '요청한 사람'에게만 보냄 (보안상)
         await sio.emit('display_otp', {'code': totp.now()}, to=sid)
 
 @sio.event
@@ -71,18 +70,13 @@ async def send_secure_msg(sid, data):
     room = data.get('room')
     msg = data.get('msg')
     if room and msg:
-        # 중요: room=room을 넣어 해당 방의 모든 sid에게 메시지 전송
-        await sio.emit('receive_secure_msg', {
-            'msg': msg, 
-            'sender': nickname 
-        }, room=room)
+        # room=room 으로 보내야 방 안의 모두에게 전달됩니다.
+        await sio.emit('receive_secure_msg', {'msg': msg, 'sender': nickname}, room=room)
 
 @sio.event
 async def leave_room(sid, data):
     room = data.get('room')
-    sio.leave_room(sid, room)
+    # 수정: await 추가
+    await sio.leave_room(sid, room)
+    if sid in user_sessions: del user_sessions[sid]
     await sio.emit('leave_success', to=sid)
-
-@app.get("/")
-async def health():
-    return {"status": "Live"}
